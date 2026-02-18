804730115c0b14ae26b87354212a9e8b013d5cb4
David Mancel <xmanced00@stud.fit.vutbr.cz>
2026-02-18 18:17:56 +0100

    gui.py: Added methods to create play/pause and next/previous buttons to view steps of the algorithm

--- gui.py
+++ gui.py
@@ -11,22 +11,52 @@ arguments:
 class AStarGUI:
 
     def __init__(self):
-
         # tkinter setup
+
+        #WINDOW
         self.window = tk.Tk()
         self.window.geometry('1000x550')
         self.window.title('GUI')
 
+        #self.window.grid_columnconfigure(1, weight=1)
 
+        #display
         self.map_frame = self.create_map_frame()
+
+        #buttons
         self.button_frame = self.create_button_frame()
+        self.create_buttons()
+        self.button_frame.rowconfigure(0, weight=1)
 
     def create_map_frame(self):
         frame = tk.Frame(self.window, height=500, bg='#FFFFFF')
         frame.pack(expand=True, fill='x')
         return frame
 
+    # BUTTONS SECTION
     #TODO: add methods to create control buttons (next/previous step, restart)
+    def create_next_btn(self):
+        btn = tk.Button(self.button_frame, text='>>')
+        btn.grid(row=0, column=3, columnspan=1, sticky=tk.NS)
+
+    def create_previous_btn(self):
+        btn = tk.Button(self.button_frame, text='<<')
+        btn.grid(row=0, column=0, columnspan=1, sticky=tk.NS)
+
+    def create_play_btn(self):
+        btn = tk.Button(self.button_frame, text='Play')
+        btn.grid(row=0, column=2, columnspan=1, sticky=tk.NS)
+
+    def create_pause_btn(self):
+        btn = tk.Button(self.button_frame, text='Pause')
+        btn.grid(row=0, column=1, columnspan=1, sticky=tk.NS)
+
+    def create_buttons(self):
+        self.create_next_btn()
+        self.create_previous_btn()
+        self.create_play_btn()
+        self.create_pause_btn()
+
     def create_button_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand=True, fill="both")
