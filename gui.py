6ae78bce87bbc73d49d2f60fc73448444e4fc7ed
David Mancel <xmanced00@stud.fit.vutbr.cz>
2026-02-18 17:51:31 +0100

    gui.py: Added methods to create map(display) frame and button frame

--- gui.py
+++ gui.py
@@ -14,9 +14,23 @@ class AStarGUI:
 
         # tkinter setup
         self.window = tk.Tk()
-        self.window.geometry('800x600')
+        self.window.geometry('1000x550')
         self.window.title('GUI')
 
+
+        self.map_frame = self.create_map_frame()
+        self.button_frame = self.create_button_frame()
+
+    def create_map_frame(self):
+        frame = tk.Frame(self.window, height=500, bg='#FFFFFF')
+        frame.pack(expand=True, fill='x')
+        return frame
+
+    #TODO: add methods to create control buttons (next/previous step, restart)
+    def create_button_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill="both")
+        return frame
 
     def run(self):
         self.window.mainloop()
